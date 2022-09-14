6bbd0eb90b8fd4eb0af6999d4dc9cabf079a5955
Amarachionye <amaragift146@gmail.com>
2022-09-14 14:51:10 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,22 +5,18 @@
  */
 void jack_bauer(void)
 {
-	 int hour, minute;
-	 hour = 0;
-	 while (hour <= 23)
-	 {
-		 minute = 0;
-		 while (minute <= 59)
-		 {
-			 _putchar('0' + hour / 10);
-			 _putchar('0' + hour % 10);
-			 _putchar(':');
-			 _putchar('0' + minute / 10);
-			 _putchar('0' + minute % 10);
-			 _putchar('\n');
-			 ++minute;
-		 }
-		 ++hour;
-	 }
+	int hr, min;
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
 
